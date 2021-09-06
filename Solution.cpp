451654d899957abfb0a29d1c1cc90b5d23c79134
albert9052 <aabb9052@gmail.com>
2021-09-07 07:22:38 +0800

    1. Added most of the main functions and started writing    Initialization().

--- Solution.cpp
+++ Solution.cpp
@@ -1,7 +1,81 @@
 #include "Solution.h"
 
+// ---------- Public ----------
+
 Solution::Solution() {
 	
 }
 
-Solution::
+void Solution::input() {
+	
+}
+
+void Solution::solve() {
+	
+}
+
+// ---------/ Public /---------
+
+// ---------- Private ----------
+// ---------- Main functions -----------
+
+std::vector<int> Solution::Initialization() {
+	
+	// Apply a preprocessing procedure to store some L nodes for each node. 
+}
+
+std::vector<int> Solution::mimicOperator(std::vector<int> oneIS) {
+	
+}
+
+std::vector<int> Solution::localSearch(std::vector<int> x) {
+	
+}
+
+std::vector<int> Solution::swallowOperator(std::vector<int> x) {
+	
+}
+
+double Solution::getObjectiveScore(std::vector<int> x) {
+	
+}
+
+void Solution::Update(std::vector<std::vector<int>>& IS, const std::vector<std::vector<int>>& Q) {
+	
+}
+
+// ---------- Other functions ----------
+
+double Solution::getStaticPreferenceValue(int currentNode, int targetNode) {
+	
+}
+
+double Solution::getDynamicPreferenceValue(const std::vector<int>& x, int node) {
+	
+}
+
+double Solution::getIncrementTime(const std::vector<int>& x, int node, int position) {
+	
+}
+
+double Solution::getRandomDecimal() {
+	
+}
+
+bool Solution::isFeasibleNode(const std::vector<int>& s, int node) {
+	
+}
+
+double Solution::getRemovalReward(const std::vector<int>& s, int node) {
+	
+}
+
+double Solution::getDistanceBetweenSolution(const std::vector<int>& x, const std::vector<int>& y) {
+	
+}
+
+// ---------/ Other functions /---------
+
+// ---------/ Main functions /---------
+
+// ---------/ Private /---------
--- Solution.h
+++ Solution.h
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <vector>
 
+// This is the size of the favorite nodes for each node, which was preprocessed
+#define L 50
+
 class Solution {
 
 public:
 	Solution();
-	input();
-	solve();
+	void input();
+	void solve();
 
 private:
-	mimicOperator();
-	localSearch();
-	swallowOperator();
+
+	// ----- Main functions ---------
+	std::vector<int> Initialization();
+	std::vector<int> mimicOperator(std::vector<int> oneIS);
+	std::vector<int> localSearch(std::vector<int> x);
+	std::vector<int> swallowOperator(std::vector<int> x);
+	double getObjectiveScore(std::vector<int> x);
+	void Update(std::vector<std::vector<int>>& IS, const std::vector<std::vector<int>>& Q);
+
+	// ----- Other functions ---------
+	double getStaticPreferenceValue(int currentNode, int targetNode);
+	double getDynamicPreferenceValue(const std::vector<int>& x, int node);
+	double getIncrementTime(const std::vector<int>& x, int node, int position);
+	// (0, 1]
+	double getRandomDecimal();
+	bool isFeasibleNode(const std::vector<int>& s, int node);
+	double getRemovalReward(const std::vector<int>& s, int node);
+	double getDistanceBetweenSolution(const std::vector<int>& x, const std::vector<int>& y);
 }
